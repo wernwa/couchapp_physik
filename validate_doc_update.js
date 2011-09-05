c0b60765193d38533ab16ca0a70fbe26109ad2bf
Walter Werner <wernwa@gmail.com>
2011-09-05 16:49:07 +0200

    pretty url

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -1,5 +1,8 @@
 function(newDoc, oldDoc, userCtx) {
-  if (userCtx.roles.indexOf('rw') !== -1) {
+  if (userCtx.roles.indexOf('rw') !== -1 
+	|| userCtx.roles.indexOf('admin') !==-1
+	|| userCtx.roles.indexOf('_admin') !==-1	// wegen cloudant
+	) {
     return;
   } else {
     throw({forbidden: 'Only users in rw group may edit the database'});
